23d86abeaf1ec2a319c24e99592028ba0cf85f0a
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-05-22 10:59:54 +0530

    detail

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
 
+const developers = [
+  { id: 1, name: 'Yash', role: 'Senior Developer' },
+  { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
+  { id: 3, name: 'Suresh', role: 'Frontend Developer' }
+];
 const AboutPage = () => {
     return (
-      <div>AboutPage</div>
+      <>
+        <h2>Developers</h2>
+      <ul>
+        {developers.map((dev) => (
+          <li key={dev.id}>
+            <Link href={`/aboutus/${dev.id}`}>{dev.name}</Link>
+          </li>
+        ))}
+      </ul>
+      </>
     )
   }
   
